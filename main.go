fa2c7e5b73ab8f39debee92db1f8b7cba408a3d1
Lahiru De Silva <lahirude@wso2.com>
2024-03-21 18:32:01 +0530

    main.go modified
    Signed-off-by: Lahiru De Silva <lahirude@wso2.com>

--- main.go
+++ main.go
@@ -7,28 +7,29 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", echoHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func echoHandler(w http.ResponseWriter, r *http.Request) {
-    // Read request body
-    decoder := json.NewDecoder(r.Body)
-    var requestBody interface{}
-    err := decoder.Decode(&requestBody)
-    if err != nil {
-        http.Error(w, "Error decoding request body", http.StatusBadRequest)
-        return
-    }
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Define a struct for the response
+	type Response struct {
+		Message string `json:"message"`
+	}
 
-    // Set Content-Type header to application/json
-    w.Header().Set("Content-Type", "application/json")
+	// Create a response object
+	response := Response{
+		Message: "Hello World",
+	}
 
-    // Encode the request body as JSON and write it to the response
-    encoder := json.NewEncoder(w)
-    err = encoder.Encode(requestBody)
-    if err != nil {
-        log.Printf("Error encoding response: %v", err)
-        http.Error(w, "Error encoding response", http.StatusInternalServerError)
-    }
+	// Set Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Encode the response object as JSON and write it to the response
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(response)
+	if err != nil {
+		log.Printf("Error encoding response: %v", err)
+		http.Error(w, "Error encoding response", http.StatusInternalServerError)
+	}
 }
